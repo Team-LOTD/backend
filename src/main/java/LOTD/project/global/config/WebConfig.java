de8608cd4927f3bf2b1f7b90028903872783641b
younjaesung <jayjoy05@khu.ac.kr>
2024-02-15 20:24:08 +0900

    members

--- src/main/java/LOTD/project/global/config/WebConfig.java
+++ src/main/java/LOTD/project/global/config/WebConfig.java
@@ -4,15 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 // 스프링 서버 전역적으로 CORS 설정
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000","15.165.236.226:8080") // 허용할 출처
+                .allowedOrigins("*") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+
                 .maxAge(3000);// 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
