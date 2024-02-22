20f60c220e97a5222ec6b6664cacf267c3992a1f
younjaesung <jayjoy05@khu.ac.kr>
2024-02-22 20:48:14 +0900

    members

--- src/main/java/LOTD/project/domain/Member/oauth2/dto/request/SocialSignUpRequest.java
+++ src/main/java/LOTD/project/domain/Member/oauth2/dto/request/SocialSignUpRequest.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class SocialSignUpRequest {
 
-    private String kakaoMemberId;
+    private String socialMemberId;
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,20}$", message = "닉네임은 특수문자를 제외한 2~20자리로 입력해주세요")
     private String nickName;
