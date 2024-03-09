fbe6b96418cb50467185511b5d6fdd4f50dd616c
younjaesung <jayjoy05@khu.ac.kr>
2024-03-09 15:59:29 +0900

    JPA Auditing 기능 추가

--- src/main/java/LOTD/project/global/audit/JpaAuditConfig.java
+++ src/main/java/LOTD/project/global/audit/JpaAuditConfig.java
@@ -1,12 +1,20 @@
 package LOTD.project.global.audit;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditConfig {
+public class JpaAuditConfig implements AuditorAware<String> {
 
-
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        String memberId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return Optional.ofNullable(memberId);
+    }
 
 }
